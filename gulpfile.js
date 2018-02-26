278e222173e0046961aac07baca63babe9fd153d
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2018-02-26 22:46:23 +0100

    Finish Gulp setup and folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,82 @@
-let gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    pump = require('pump');
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
 
-gulp.task('compress', function (cb) {
-  pump([
-      gulp.src('js/*.js'),
-      uglify(),
-      gulp.dest('js/min')
-  ],
-  cb
-  );
-});
+var paths = {
+  styles: {
+    src: 'build/css/**/*.css',
+    dest: 'dist/css/'
+  },
+  scripts: {
+    src: 'build/js/**/*.js',
+    dest: 'dist/js/'
+  }
+};
 
-gulp.task('default', () => {
-  gulp.start('compress');
-});
+/* Not all tasks need to use streams, a gulpfile is just another node program
+ * and you can use all packages available on npm, but it must return either a
+ * Promise, a Stream or take a callback and call it
+ */
+function clean() {
+  // You can use multiple globbing patterns as you would with `gulp.src`,
+  // for example if you are using del 2.0 or above, return its promise
+  return del([ 'dist' ]);
+}
+
+// Move html files from build to dist
+function move() {
+  return gulp.src('build/*.html')
+    .pipe(gulp.dest('dist/'));
+}
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(cleanCSS())
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
+
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+exports.move = move;
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.series(clean, move, gulp.parallel(styles, scripts));
+
+/*
+ * You can still use `gulp.task` to expose tasks
+ */
+gulp.task('build', build);
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+gulp.task('default', build);
