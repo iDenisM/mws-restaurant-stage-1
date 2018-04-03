d0fed3354e0c831184677c2bd3f9b2bb97e3437d
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2018-04-03 12:45:35 +0200

    Minor adjustments before release

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -71,8 +71,6 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   fillReviewsHTML();
 }
 
-/* TODO: Create navigation for table menu
-*/
 /**
  * Create restaurant operating hours HTML table and add it to the webpage.
  */
@@ -101,7 +99,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
-  title.tabindex = 0;
+  title.tabIndex = 0;
   container.appendChild(title);
 
   if (!reviews) {
